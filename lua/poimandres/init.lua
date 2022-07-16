0adb2478e67b3a0c8d9c95876036ffa4d4c24c55
Oliver Cederborg <hey@olivercederborg.com>
2022-07-16 21:59:38 +0200

    feat: initialize theme configuration

---
+++ lua/poimandres/init.lua
@@ -0,0 +1,91 @@
+local utils = require('poimandres.utils')
+
+local M = {}
+
+local config = {
+	bold_vert_split = false,
+	dark_variant = 'main',
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = false,
+
+	groups = {
+		background = 'background3',
+		panel = 'background2',
+		border = 'background1',
+		comment = 'blue4',
+		link = 'blue1',
+		punctuation = 'blueGray2',
+
+		error = 'pink3',
+		hint = 'pink2',
+		info = 'blue2',
+		warn = 'yellow',
+
+		git_add = 'teal1',
+		git_change = 'pink2',
+		git_delete = 'pink3',
+		git_dirty = 'pink2',
+		git_ignore = 'blueGray2',
+		git_merge = 'blue2',
+		git_rename = 'teal2',
+		git_stage = 'blue1',
+		git_text = 'teal1',
+
+		headings = {
+			h1 = 'teal1',
+			h2 = 'yellow',
+			h3 = 'pink3',
+			h4 = 'pink2',
+			h5 = 'blue1',
+			h6 = 'blue2',
+		},
+	},
+	highlight_groups = {},
+}
+
+function M.setup(opts)
+	opts = opts or {}
+	vim.g.poimandres_variant = opts.dark_variant or 'main'
+
+	if opts.groups and type(opts.groups.headings) == 'string' then
+		opts.groups.headings = {
+			h1 = opts.groups.headings,
+			h2 = opts.groups.headings,
+			h3 = opts.groups.headings,
+			h4 = opts.groups.headings,
+			h5 = opts.groups.headings,
+			h6 = opts.groups.headings,
+		}
+	end
+
+	config.user_variant = opts.dark_variant or nil
+	config = vim.tbl_deep_extend('force', config, opts)
+end
+
+function M.colorscheme()
+	if vim.g.colors_name then
+		vim.cmd('hi clear')
+	end
+
+	vim.opt.termguicolors = true
+	vim.g.colors_name = 'poimandres'
+
+	local theme = require('poimandres.theme').get(config)
+
+	-- Set theme highlights
+	for group, color in pairs(theme) do
+		-- Skip highlight group if user overrides
+		if config.highlight_groups[group] == nil then
+			utils.highlight(group, color)
+		end
+	end
+
+	-- Set user highlights
+	for group, color in pairs(config.highlight_groups) do
+		utils.highlight(group, color)
+	end
+end
+
+return M
---
+++ lua/poimandres/palette.lua
@@ -0,0 +1,33 @@
+local variants = {
+	main = {
+		yellow = '#FFFAC2',
+		teal1 = '#5DE4C7',
+		teal2 = '#5FB3A1',
+		teal3 = '#42675A',
+		blue1 = '#89DDFF',
+		blue2 = '#ADD7FF',
+		blue3 = '#91B4D5',
+		blue4 = '#7390AA',
+		pink1 = '#FAE4FC',
+		pink2 = '#FCC5E9',
+		pink3 = '#D0679D',
+		blueGray1 = '#A6ACCD',
+		blueGray2 = '#767C9D',
+		background1 = '#506477',
+		background2 = '#303340',
+		background3 = '#1B1E28',
+		text = '#E4F0FB',
+		gray = '#2A2C34',
+		none = 'NONE',
+	},
+}
+
+local palette = {}
+
+if vim.o.background == "light" then
+	palette = variants.main
+else
+	palette = variants[(vim.g.poimandres_variant == "storm" and "storm") or "main"]
+end
+
+return palette
