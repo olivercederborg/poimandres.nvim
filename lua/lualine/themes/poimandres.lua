716615013c7409185085ef9b888242d3040938a2
Oliver Cederborg <hey@olivercederborg.com>
2022-07-18 00:01:40 +0200

    feat(lualine): add lualine theme

---
+++ lua/lualine/themes/poimandres.lua
@@ -0,0 +1,37 @@
+local palette = require('poimandres.palette')
+
+local poimandres = {}
+
+poimandres.normal = {
+	a = { fg = palette.background2, bg = palette.teal1, gui = 'bold' },
+	b = { fg = palette.text, bg = palette.background1 },
+	c = { fg = palette.blueGray1, bg = palette.none },
+}
+
+poimandres.insert = {
+	a = { fg = palette.background2, bg = palette.yellow, gui = 'bold' },
+	b = { fg = palette.text, bg = palette.background1 },
+}
+
+poimandres.visual = {
+	a = { fg = palette.background2, bg = palette.blue1, gui = 'bold' },
+	b = { fg = palette.text, bg = palette.background1 },
+}
+
+poimandres.replace = {
+	a = { fg = palette.background2, bg = palette.pink3, gui = 'bold' },
+	b = { fg = palette.text, bg = palette.background1 },
+}
+
+poimandres.command = {
+	a = { fg = palette.background2, bg = palette.yellow, gui = 'bold' },
+	b = { fg = palette.text, bg = palette.background1 },
+}
+
+poimandres.inactive = {
+	a = { fg = palette.blueGray3, bg = palette.background1, gui = 'bold' },
+	b = { fg = palette.blueGray3, bg = palette.background1 },
+	c = { fg = palette.blueGray3, bg = palette.none },
+}
+
+return poimandres
